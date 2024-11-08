72ee88887da16a8bb392be51ac347f0168de9edf
dontgetmad <168203658+dontgetmad@users.noreply.github.com>
2024-11-08 23:25:05 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-
+function generateBarcode() {
+    barcodeContainer.innerHTML = '';
+    input = document.getElementById("input").value;
+    const lines = input.split('\n').filter(line => line.trim() !== '');
+    try {
+        
+        for (let i  = 0; i < lines.length; i++) {
+            const barcodeDiv = document.createElement('div');
+            barcodeDiv.innerHTML = `<svg id="barcode${i}"></svg>`;
+            barcodeContainer.appendChild(barcodeDiv);
+            JsBarcode(`#barcode${i}`, lines[i]);
+        }
+    } catch (error) {
+        console.log("Error generating barcode:", error);
+    }
+    console.log(`${lines.length} barcodes generated`);
+}
